73e39aefd7e48c0deacfdddd9b0e03f37c43223c
Barley <barley@honbabin.com>
2018-06-20 17:38:01 +0900

    Inheritance

--- src/TestClass.kt
+++ src/TestClass.kt
@@ -1,6 +1,8 @@
 fun main(args: Array<String>) {
     val obj = Customer("코틀린")
     println("------ "+obj.name)
+    val obj2 = BB(10)
+    obj2.f()
 }
 
 class Customer(var name: String) {
@@ -10,8 +12,27 @@ class Customer(var name: String) {
     }*/
 }
 
-class Person {
+class Person: Any {
     constructor(parent: Person) {
 //        parent.children.add(this)
     }
+}
+
+// open : final 반대
+open class AA(x: Int) {
+    open fun f() {
+        println("AAAA")
+    }
+}
+
+class BB(x: Int) : AA(x), CC {
+    override fun f() {
+        super<AA>.f()
+        super<CC>.f()
+    }
+}
+
+interface CC {
+    fun f() { println("CCCC") }
+    fun b() { }
 }
