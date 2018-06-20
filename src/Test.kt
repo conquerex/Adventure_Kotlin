6f78e2abd31a2dde2853b277b74cbb635def81da
Barley <barley@honbabin.com>
2018-06-20 15:49:53 +0900

    Packages, return and jumps

--- src/Test.kt
+++ src/Test.kt
@@ -2,6 +2,9 @@
 fun main(args: Array<String>) {
     showKotiln()
     testBasicTypes()
+    iterater10()
+    showForEach()
+    showForEach2()
 }
 
 fun showKotiln(): Unit {
@@ -30,5 +33,39 @@ fun testBasicTypes() {
     val max = if(a>b) a else b
 
     when : Switch case문
-    boolean은 else가 없어도 됨
+    boolean은 else가 없어도
+    range or collection 사용 가능
+    스마트 캐스트
  */
+
+fun iterater10() {
+
+    loop@ for (i in 1..10) {
+        println("---- i: $i ---")
+
+        for (j in 1..10) {
+            println("----- j: $j ---")
+            if (i+j > 12) {
+                break@loop
+            }
+        }
+    }
+}
+
+fun showForEach() {
+    var ints = listOf(0,1,2,3)
+    ints.forEach label@{
+        if (it == 1) return@label
+        print(it)
+    }
+    println("End")
+}
+
+fun showForEach2() {
+    var ints = listOf(0,1,2,3)
+    ints.forEach {
+        if (it == 1) return@forEach
+        print(it)
+    }
+    println("End")
+}
