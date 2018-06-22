08cffd6b360a89b811d67ed6c374e9ab59e401ca
Barley <barley@honbabin.com>
2018-06-22 11:57:38 +0900

    Object expressions and Declarations

--- src/TestObject.kt
+++ src/TestObject.kt
@@ -10,6 +10,7 @@ class MyClass {
     fun publicFun() = object {
         var x = 12
     }
+
     private fun privateFun() = object {
         var x = 45
     }
@@ -17,6 +18,24 @@ class MyClass {
     fun print() {
 //        println(publicFun().x) // Error
         println(privateFun().x)
+    }
+}
+
+object YourClass1 {
+    init {
+        println("* * * * Your class 1")
+    }
+}
+
+object YourClass2 {
+    init {
+        println("* * * * Your class 2")
+    }
+}
+
+object YourClass3 {
+    init {
+        println("* * * * Your class 3")
     }
 }
 
@@ -37,4 +56,33 @@ fun main(args: Array<String>) {
 
     println("------ ${data.x}, ${data.y}")
     MyClass().print()
+
+    /**
+     * 이렇게는 사용 불가
+    var a = object Counter {
+        var count = 0
+    }
+    */
+
+    CounterManager.count++
+    CounterManager.count++
+    println(CounterManager.count)
+
+    // Singleton Test
+    println(1)
+    println(YourClass1)
+    println(YourClass1)
+    println(YourClass1)
+    println(2)
+    println(YourClass2)
+    println(YourClass2)
+    println(YourClass2)
+    println(3)
+    println(YourClass3)
+    println(4)
+
+}
+
+object CounterManager {
+    var count = 0
 }
